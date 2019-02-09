e4d84936cc9bfa109b7d03975cbd58a4241d3d8f
Helder Burato Berto <helder.burato@gmail.com>
2019-02-09 16:25:54 -0200

    chore: move all logic to useLocation hook

--- src/components/molecules/Form/useLocation.js
+++ src/components/molecules/Form/useLocation.js
@@ -1,16 +1,50 @@
 import { useState } from "react";
 
+import ViaCep from "services/viacep";
+
 export default () => {
   const [zipcode, setZipcode] = useState("");
   const [location, setLocation] = useState("");
   const [invalidZipcode, setStatusZipcode] = useState(false);
 
+  const clearResults = () => {
+    setLocation("");
+    setZipcode("");
+    setStatusZipcode(false);
+  };
+
+  const handleBlur = () => {
+    const ZIPCODE_WITHOUT_DASH_SIZE = 8;
+
+    if (zipcode.length < ZIPCODE_WITHOUT_DASH_SIZE) {
+      setStatusZipcode(true);
+      return;
+    }
+
+    if (
+      zipcode.length >= ZIPCODE_WITHOUT_DASH_SIZE &&
+      !ViaCep.isValidZipcode(zipcode)
+    ) {
+      const maskedZipcode = ViaCep.maskZipcode(zipcode);
+      setZipcode(maskedZipcode);
+      setStatusZipcode(false);
+    }
+  };
+
+  const updateLocation = ({ location, invalidZipcode }) => {
+    setLocation(location);
+    setStatusZipcode(invalidZipcode);
+  };
+
+  const updateZipcode = ({ zipcode }) => setZipcode(zipcode);
+
   return [
     zipcode,
-    setZipcode,
     location,
-    setLocation,
     invalidZipcode,
-    setStatusZipcode
+    clearResults,
+    handleBlur,
+    updateLocation,
+    updateZipcode
   ];
 };
